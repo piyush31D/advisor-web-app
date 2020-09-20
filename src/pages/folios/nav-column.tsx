b2d9cb526ca650dd9725f532fe312bd30b57208e
abhishek bhaskar <abhikbhasker@gmail.com>
2020-09-20 10:40:57 +0530

    nav-column component rename

---
+++ src/pages/folios/nav-column.tsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import cx from 'classnames'
+import styles from './nav-column.module.css';
+import Button from 'src/components/button/text.button'
+
+const NavColumn: React.FC = () => {
+  return (
+    <div className={cx(styles.navbar, 'flex col-flex cross-stretch')}>
+      <NavLink to="/folios/all" className={cx(styles.navlink, 'flex row-flex space-between cross-center')} activeClassName={cx(styles.activeNavlink)}>
+        <span>All Folios</span>
+        <span className={styles.navlinkBadge}>103</span>
+      </NavLink>
+      <NavLink to="/editfolio" className={cx(styles.navlink, 'flex row-flex space-between cross-center')} activeClassName={cx(styles.activeNavlink)} >
+        <span>New folio</span>
+        <i className="pficon-plus font-medium"/>
+      </NavLink>
+      <div className={styles.navTabBar}>
+        <div className={cx(styles.navTab, styles.activeNavTab)}>
+          Folios
+        </div>
+        <div className={styles.navTab}>
+          Pinned
+        </div>
+      </div>
+      <NavLink to="/folios/1" activeClassName={styles.activeGroupCard} className={styles.groupCard}>
+        <p>Folio name</p>
+        <div className="flex row-flex space-between cross-center">
+          <span>.</span>
+          <div className={styles.groupCardMenu}>
+            <Button type="text-white" size="regular" icon="menu-overflow" />
+          </div>
+        </div>
+      </NavLink>
+      <NavLink to="/folios/2" activeClassName={styles.activeGroupCard} className={styles.groupCard}>
+        <p>Telecom, brand low risk</p>
+        <div className="flex row-flex space-between cross-center">
+        <span>.</span>
+          <div className={styles.groupCardMenu}>
+            <Button type="text-white" size="regular" icon="menu-overflow" />
+          </div>
+        </div>
+      </NavLink>
+      <NavLink to="/folios/3" activeClassName={styles.activeGroupCard} className={styles.groupCard}>
+        <p>Some random folio name</p>
+        <div className="flex row-flex space-between cross-center">
+        <span>.</span>
+          <div className={styles.groupCardMenu}>
+            <Button type="text-white" size="regular" icon="menu-overflow" />
+          </div>
+        </div>
+      </NavLink>
+      <NavLink to="/folios/4" activeClassName={styles.activeGroupCard} className={styles.groupCard}>
+        <p>Intraday test folio</p>
+        <div className="flex row-flex space-between cross-center">
+        <span>.</span>
+          <div className={styles.groupCardMenu}>
+            <Button type="text-white" size="regular" icon="menu-overflow" />
+          </div>
+        </div>
+      </NavLink>
+    </div>
+  );
+};
+
+export default NavColumn;
