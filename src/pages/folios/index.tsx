e0507a8b34224a137fce30fed258f41956fbba0f
abhishek bhaskar <abhikbhasker@gmail.com>
2020-09-20 10:40:01 +0530

    rename folio page component

---
+++ src/pages/folios/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import NavColumn from './nav-column'
+import styles from './index.module.css';
+import FoliosPageRoute from 'src/routes/folios.page';
+
+const FoliosPage: React.FC = () => {
+  return (
+    <div className="flex fill row-flex cross-stretch">
+      <NavColumn />
+      <div className={styles.container}>
+        <FoliosPageRoute/>
+      </div>
+    </div>
+  );
+};
+
+export default FoliosPage;
