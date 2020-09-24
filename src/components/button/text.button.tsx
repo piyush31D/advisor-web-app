20ec1a3badda5d027c3a45a64d08ac0b15c306a9
abhishek bhaskar <abhikbhasker@gmail.com>
2020-09-24 17:10:17 +0530

    update text button to follow themes

--- src/components/button/text.button.tsx
+++ src/components/button/text.button.tsx
@@ -4,19 +4,22 @@ import styles from './text-button.module.css';
 
 interface Props {
   title?: string,
-  type: 'solid' | 'text-accent' | 'text-primary'| 'text-grey'|'text-red'|'text-white'|'fill-accent',
-  round?:boolean,
+  type: 'solid' | 'text-accent' | 'text-primary' | 'text-grey' | 'text-red' | 'text-white' | 'fill-accent',
+  round?: boolean,
+  thick?: boolean,
   size: 'regular' | 'medium' | 'large',
   icon?: string,
   iconRight?: boolean | true,
-  onClick?:(event: React.MouseEvent<HTMLButtonElement>) => void;
+  onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
 }
 const Button: React.FC<Props> = (props) => {
   return (
-    <button onClick={props.onClick} className={cx(styles.button, styles[props.type], styles[props.size],props.round&&styles.round)}>
-      {props.title && props.title}
-      {props.icon && props.title && <span className={styles.spacer} />}
-      {props.icon && <i className={cx(`pficon-${props.icon}`, styles[props.size])} />}
+    <button onClick={props.onClick} className={cx(styles.button, styles[props.type], styles[props.size], props.round && styles.round, props.thick && styles.thick)}>
+      <span className={styles.buttonText}>
+        {props.title && props.title}
+        {props.icon && props.title && <span className={styles.spacer} />}
+        {props.icon && <i className={cx(`pficon-${props.icon}`, styles[props.size])} />}
+      </span>
     </button>
   );
 };
