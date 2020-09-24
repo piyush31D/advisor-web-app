1825d1b76abaa439f82351ae5685f37247ce8f0b
abhishek bhaskar <abhikbhasker@gmail.com>
2020-09-24 17:12:29 +0530

    update nav columns to follow theme

--- src/pages/investor/nav-column.tsx
+++ src/pages/investor/nav-column.tsx
@@ -1,27 +1,11 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import Button from '@material-ui/core/Button';
 import { createStyles, makeStyles } from '@material-ui/core/styles';
 import cx from 'classnames';
 import TextButton from 'src/components/button/text.button';
 import styles from './index.module.css';
 
-const useStyles = makeStyles(() =>
-  createStyles({
-    compactButton: {
-      padding: '2px 6px',
-      minWidth: 0,
-      borderRadius: 6
-    },
-    buttonIconSmall: {
-      fontSize: '12px !important'
-    }
-  }),
-);
-
 const NavColumn: React.FC = () => {
-  const classes = useStyles();
-
   return (
     <div className={cx(styles.navbar, 'flex col-flex cross-stretch')}>
       <NavLink to="/investor/all" className={cx(styles.navlink, 'flex row-flex space-between cross-center')} activeClassName={cx(styles.activeNavlink)}>
@@ -33,11 +17,9 @@ const NavColumn: React.FC = () => {
         <span className={styles.navlinkBadge}>46</span>
       </NavLink>
       <div className={styles.groupsWrap}>
-        <div className="flex row-flex cross-center space-between text-white semi-bold font-medium">
+        <div className="flex row-flex cross-center space-between text-white semi-bold font-medium margin-bottom">
           <span style={{ paddingLeft: 5 }}>Groups</span>
-          <Button className={classes.compactButton} endIcon={<span className={cx(classes.buttonIconSmall, 'pficon-plus')} />}>
-            New
-          </Button>
+          <TextButton type="text-white" size="medium" icon="plus" />
         </div>
         <div className={cx(styles.groupCard)}>
           <p>Show all groups</p>
