f4fae0b2d2bfb37c83332b9996e1476aa9e84e3c
abhishek bhaskar <abhikbhasker@gmail.com>
2020-09-30 22:53:03 +0530

    update folio page with reusable navcolum component

--- src/pages/folio/index.tsx
+++ src/pages/folio/index.tsx
@@ -1,12 +1,45 @@
-import React from 'react';
-import NavColumn from './nav-column'
 import styles from './index.module.css';
-import FoliosPageRoute from 'src/routes/folio';
+import React from 'react';
+import cx from 'classnames';
+import FoliosPageRoute from 'src/routes/folio'; import NavColumn, { NavColumnLink, NavColumnCard } from 'src/components/navcolumn/nav-column';
 
 const FoliosPage: React.FC = () => {
+  const folioCount = 13;
+
+  function createGroupData(_id: string, name: string) {
+    return { _id, name, };
+  }
+
+  const Folios = [
+    createGroupData('121', 'Random Folio Name'),
+    createGroupData('122', 'Options high risk Low budget'),
+    createGroupData('123', 'Goal home high budget'),
+  ];
   return (
     <div className="flex fill row-flex cross-stretch">
-      <NavColumn />
+      <NavColumn>
+        <NavColumnLink linkTo="/folio/all" title="All Folios" badgeText={folioCount.toString()} />
+        <NavColumnLink linkTo="/folio/new" title="New Folio" icon="plus" />
+        <div className={styles.navTabBar}>
+          <div className={cx(styles.navTab, styles.activeNavTab)}>
+            Folios
+        </div>
+          <div className={styles.navTab}>
+            Pinned
+        </div>
+        </div>
+        {Folios.map((folio, index) => (
+          <NavColumnCard
+            linkTo={`/folio/${folio._id}`}
+            name={folio.name}
+            brief={
+              <span>
+                .
+              </span>
+            }
+          />
+        ))}
+      </NavColumn>
       <div className={styles.container}>
         <FoliosPageRoute />
       </div>
