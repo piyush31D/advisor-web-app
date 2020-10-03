7e121071a75f9b910a55c96b8d460aead16339ba
abhishek bhaskar <abhikbhasker@gmail.com>
2020-10-03 12:22:29 +0530

    update text button

--- src/components/button/text.button.tsx
+++ src/components/button/text.button.tsx
@@ -3,22 +3,37 @@ import cx from 'classnames';
 import styles from './text-button.module.css';
 
 interface Props {
-  title?: string,
-  type: 'solid' | 'text-accent' | 'text-primary' | 'text-grey' | 'text-red' | 'text-white' | 'fill-accent'|'fill-primary',
-  round?: boolean,
-  thick?: boolean,
-  size: 'regular' | 'medium' | 'large',
-  icon?: string,
-  iconRight?: boolean | true,
+  type?: 'button' | 'submit' | 'reset';
+  title?: string
+  variant: 'solid' | 'text-accent' | 'text-primary' | 'text-grey' | 'text-red' | 'text-white' | 'fill-accent' | 'fill-primary';
+  round?: boolean;
+  thick?: boolean;
+  size: 'regular' | 'medium' | 'large';
+  icon?: string;
+  iconRight?: boolean | true;
+  fullWidth?:boolean;
   onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
 }
 const Button: React.FC<Props> = (props) => {
   return (
-    <button onClick={props.onClick} className={cx(styles.button, styles[props.type], styles[props.size], props.round && styles.round, props.thick && styles.thick)}>
-      <span className={styles.buttonText}>
+    <button
+      type={props.type}
+      onClick={props.onClick}
+      className={cx(styles.button, styles[props.variant], styles[props.size], props.round && styles.round, props.thick && styles.thick,props.fullWidth&&styles.fullWidth)}
+    >
+      <span
+        className={styles.buttonText}
+      >
         {props.title && props.title}
-        {props.icon && props.title && <span className={styles.spacer} />}
-        {props.icon && <i className={cx(`pficon-${props.icon}`, styles[props.size])} />}
+        {props.icon && props.title &&
+          <span
+            className={styles.spacer}
+          />
+        }
+        {props.icon &&
+          <i className={cx(`pficon-${props.icon}`, styles[props.size])}
+          />
+        }
       </span>
     </button>
   );
